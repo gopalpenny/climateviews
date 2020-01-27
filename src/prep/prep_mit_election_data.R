a1f552a1287c46ee12711613cf54dbf6be251117
Gopal <gopalpenny@gmail.com>
2020-01-26 23:39:06 -0500

    Added prep_mit_election_data.R

---
+++ src/prep/prep_mit_election_data.R
@@ -0,0 +1,5 @@
+# prep_mit_election_data.R
+
+# https://electionlab.mit.edu/data
+# https://github.com/MEDSL/2018-elections-unoffical
+mit_election <- read_csv("https://raw.githubusercontent.com/MEDSL/2018-elections-unoffical/master/election-context-2018.csv")
