84c358e593f7f97aa6133d77221d20a3dbd53d10
Gopal <gopalpenny@gmail.com>
2020-01-27 23:47:13 -0500

    added presidential vote percentage for clinton16 and obama12

--- src/prep/prep_mit_election_data.R
+++ src/prep/prep_mit_election_data.R
@@ -2,6 +2,9 @@
 
 # https://electionlab.mit.edu/data
 # https://github.com/MEDSL/2018-elections-unoffical
-mit_election <- read_csv("https://raw.githubusercontent.com/MEDSL/2018-elections-unoffical/master/election-context-2018.csv")
+mit_election <- read_csv("https://raw.githubusercontent.com/MEDSL/2018-elections-unoffical/master/election-context-2018.csv") %>% 
+  mutate(obama12_pct= 100 * obama12 / (romney12 + obama12),
+         clinton16_pct= 100 * clinton16 / (trump16 + clinton16),
+         urban_pct = 100 - rural_pct)
 
 write_csv(mit_election,"data/format/mit_election_18.csv")
