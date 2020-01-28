6c1d5c62879a645cda615a0bc38e4f03397fb329
Gopal <gopalpenny@gmail.com>
2020-01-28 13:01:03 -0500

    convert fips to 5-character string

--- src/scrape/scrape_county_fips.R
+++ src/scrape/scrape_county_fips.R
@@ -8,8 +8,10 @@ us_fips_codes <- us_fips %>%
   rvest::html_node(xpath="//*[@id=\"detail\"]/table") %>%
   rvest::html_table()
 
-us_fips_codes_format <- us_fips_codes %>%
-  mutate(county_lower=gsub("[\\. ]","",tolower(Name)),
-         county_lower=ifelse(State=="VA",gsub("city$","",county_lower),county_lower))
+us_fips_codes_format <- us_fips_codes %>% rename(fips=FIPS) %>%
+  mutate(FIPS=ifelse(str_length(fips)==4,paste0("0",fips),paste0(fips)),
+         county_lower=gsub("[\\. ]","",tolower(Name)),
+         county_lower=ifelse(State=="VA",gsub("city$","",county_lower),county_lower)) %>%
+  select(FIPS,everything()) %>% as_tibble()
 
 write_csv(us_fips_codes_format,"data/format/usa_county_fips_codes.csv")
