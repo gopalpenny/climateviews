f972aa5e3f7918afbce32de50b8cb03c7e8dfb0f
Gopal <gopalpenny@gmail.com>
2020-01-30 14:57:34 -0500

    updated overview.html and incubator_date.csv

--- src/prep/prep_pew_surveys_incomplete.R
+++ src/prep/prep_pew_surveys_incomplete.R
@@ -2,7 +2,7 @@ library(haven)
 library(tidyverse)
 
 #### WAVE 39
-path <- "~/Downloads/W39_Nov18/ATP W39.sav"
+path <- "data/orig/pew/W39_Nov18/ATP W39.sav"
 file.exists(path)
 
 pew_w39 <- read_sav(path)
@@ -10,7 +10,13 @@ pew_w39 <- read_sav(path)
 n <- ncol(pew_w39)
 labels_list <- map(1:n, function(x) attr(pew_w39[[x]], "label") )
 # if a vector of character strings is preferable
+colnames_vector <- names(pew_w39)
+colnames_vector[108]
 labels_vector <- map_chr(1:ncol(pew_w39), function(x) attr(pew_w39[[x]], "label") )
+# questions <- tibble(q=labels_vector)
+# write_csv(questions,"data/format/W39_Nov18_questions.csv")
+pew_w39 %>% select(108)
+
 pew_w39 %>% select(c(1,grep("eight",labels_vector)))
 
 ggplot(pew_w39) + geom_histogram(aes(WEIGHT_W39_FINAL))
@@ -21,7 +27,7 @@ table(a$LRNGFPO_W39)
 
 ### SEPT 18
 
-path <- "~/Downloads/Sept18/Sept18 public.sav"
+path <- "data/orig/pew/Sept18/Sept18 public.sav"
 sept18 <- read_sav(path)
-labels_vector <- map_chr(1:ncol(sept18), function(x) attr(sept18[[x]], "label") )git 
+labels_vector <- map_chr(1:ncol(sept18), function(x) attr(sept18[[x]], "label") )
 labels_vector[grepl("district",labels_vector)]
